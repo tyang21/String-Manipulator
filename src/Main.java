474b17cbe2f6148a238186cd8cd174631a5f2d37
tyang21 <42978521+tyang21@users.noreply.github.com>
2018-10-17 14:30:33 -0400

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Boolean tryAgain = true;
+		StringManipulator s = new StringManipulator();
+		Scanner in = new Scanner(System.in);
+		System.out.println("Choose  method:"); //Display Prompt
+		String m = in.next();
+		in.nextLine();
+		while (tryAgain = false) 
+		{
+			System.out.println();
+		}
+
+	}
+
+}
--- src/StringManipulator.java
+++ src/StringManipulator.java
@@ -1,4 +1,37 @@
-
-public class StringManipulator {
-
+/**
+ * String manipulator
+ * @author Tyler Yang
+ * Period 7
+ */
+public class StringManipulator 
+{
+	public String noVowels(String str) 
+	{
+		String out = "";
+		
+		for(int i = 0; i < str.length(); i++) 
+		{
+			if (str.charAt(i) == 'a' || str.charAt(i) == 'e' || str.charAt(i) == 'e' || str.charAt(i) == 'i' || str.charAt(i) == 'o' ||  str.charAt(i) == 'u')
+			{
+				out += "*";
+			}
+			else
+			{
+				out += str.charAt(i);
+			}
+		}
+		return out;
+	}
+	
+	public String reverse(String str1) 
+	{
+		String str2 = "";
+		
+		for (int i = str1.length() - 1; i >= 0; i--) 
+		{
+			str2 +=  str1.charAt(i);
+		}
+		
+		return str2;			
+	}
 }
